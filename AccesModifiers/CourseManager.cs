3beef87a74161074d223c452f1d9643f3f9c8900
ELİF BÖLÜK <elifbolukk@gmail.com>
2022-05-19 14:59:41 +0300

    Acces Modifiers

---
+++ AccesModifiers/CourseManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccesModifiers
+{
+    internal class CourseManager
+    {
+        public void Add()
+        {
+            Course course = new Course();
+            {
+                // private sadece o sınıf içinde tanımlanmışsa o sınıfta, metod içinde tanımlanmışsa o metodta, hatta if bloğu içindeyse sadece o if bloğu içerisinde geçerlidir. Kısacası tanımlandığı blok içerisinde geçerlidir.
+                // protected private gibi kullanılıyor ve aynı zamanda inheritance aldığı yerde de geçerlidir.
+                // internal ise bulunduğu proje içerisinde istediğimiz noktada çağırabileceğimiz anlamına gelir.
+                // public ise referans verildiği zaman başka projede kısacası herhangi bir erişim kısıtını koymak istemediğimiz zaman kullanabiliyoruz.
+
+                // Kodun okunurluğunu arttırmak ve programcıyı yanlış yönlendirmemek için classlarda hangi Acces Modifiers gerekiyorsa o verilmedilir. 
+            }
+        }
+    }
+}
--- AccesModifiers/Program.cs
+++ AccesModifiers/Program.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 // Private erişim bildirgeci sadece tanımlandığı blok içerisinde geçerli olan yapıdır. (class, field, property  olabilir, her şeyi private yapabiliriz.)
 
 
-namespace AccesModifiers // (ErişimBildirgeçleri) private - protected
+namespace AccesModifiers // (ErişimBildirgeçleri) private → protected → internal
 {
     internal class Program
     {
@@ -34,7 +34,23 @@ namespace AccesModifiers // (ErişimBildirgeçleri) private - protected
             // Customer customer = new Customer();
             // customer.id; // id gelmez çünkü id private bir değişkendir. private değişken sadece tanımlandığı blok içerisinde geçerlidir.
 
-            Id // Protected'da ise Id buraya gelir. Protected ve private farkı: protected olarak tanımladığımız değişken; özellik, metod her ne ise.. inherite edildiği sınıflarda kullanılabilir, yani private'ı inheritence sınıfına çıkarabiliyoruz.
+            // Id // Protected'da ise Id buraya gelir. Protected ve private farkı: protected olarak tanımladığımız değişken; özellik, metod her ne ise.. inherite edildiği sınıflarda kullanılabilir, yani private'ı inheritence sınıfına çıkarabiliyoruz.
         }
+
     }
+
+    internal class Course // class'ların default acces modifier'ı private, public veya protected değil. Bir class'ın defaultu internal'dır.
+    // internal bir class'ı bağlı bulunduğu proje (assembly proje denir buna) içerisinde referans ihtiyacı olmadan kullanabiliriz.
+    {
+        // Course class'ı CourseManager.cs içerisinde çağırıldı.
+        public string Name { get; set; }
+        private class NestedClass
+        {
+            // private class sadece Course class'ı içerisinde kullanılır.
+        }
+
+        // Course class'ını farklı bir proje içerisinde de kullanmak istersek public yapmamız yeterlidir.
+    }
+
+
 }
