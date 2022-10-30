e4ab2c2ebf388d15807b462081daabcaa144baa8
ELİF BÖLÜK <elifbolukk@gmail.com>
2022-10-30 22:39:54 +0300

    ADO.NET

--- AdoNetDemo/ProductDal.cs
+++ AdoNetDemo/ProductDal.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -17,6 +18,13 @@ namespace AdoNetDemo
         {
 
             SqlConnection connection = new SqlConnection(@"server=(localdb)\mssqllocaldb;initial catalog=ETrade; integrated security=true"); // @ burada yazdığım her şeyi tamamen bir string kabul et demektir.
+            if (connection.State==ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+            SqlCommand command = new SqlCommand("Select * from Products");
+
+
         }
     }
 }
