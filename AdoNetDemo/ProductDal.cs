98133a7b83a469f27ef482f2ca7720842eb45fba
ELİF BÖLÜK <elifbolukk@gmail.com>
2022-10-31 23:23:32 +0300

    ADO.NET

--- AdoNetDemo/ProductDal.cs
+++ AdoNetDemo/ProductDal.cs
@@ -25,13 +25,40 @@ namespace AdoNetDemo
             SqlCommand command = new SqlCommand("Select * from Products", connection); // Sorguyu bağlantıya gönder.
             SqlDataReader reader = command.ExecuteReader(); // ExecuteReader metodu ile komut çalıştırılır. 
 
-            // DataTable dataTable = new DataTable();
-            // dataTable.Load(reader);
-            // reader.close();
-            // connection.close();
-            // return dataTable();
+            List<Product> products=new List<Product>();
+            while (reader.Read()); // reader'daki kayıtları tek tek oku, okuyabildiğin sürece döngü içerisinde çalıştır. 
+            {
+                Product product = new Product
+                {
+                    Id = Convert.ToInt32(reader["Id"]),
+                    Name = reader["Name"].ToString(),
+                    StockAmount = Convert.ToInt32(reader["StockAmount"]),
+                    UnitPrice = Convert.ToDecimal(reader["UnitPrice"])
+                };
+                products.Add(product); // Her okuduğun elemanı product'a aktar, sonra da listeye ekle. 
+            }
+            reader.Close();
+            connection.Close();
+            return products;
+        }
 
 
+        public DataTable GetAll2() // Ürünleri listeleyeceğimiz method.
+        {
+
+            SqlConnection connection = new SqlConnection(@"server=(localdb)\mssqllocaldb;initial catalog=ETrade; integrated security=true"); // @ burada yazdığım her şeyi tamamen bir string kabul et demektir.
+            if (connection.State == ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+            SqlCommand command = new SqlCommand("Select * from Products", connection); // Sorguyu bağlantıya gönder.
+            SqlDataReader reader = command.ExecuteReader(); // ExecuteReader metodu ile komut çalıştırılır. 
+
+            DataTable dataTable = new DataTable();
+            dataTable.Load(reader);
+            reader.Close();
+            connection.Close();
+            return dataTable;
         }
     }
 }
