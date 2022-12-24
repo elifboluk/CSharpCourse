941b6a546d3f6c05aad510edf48155b55a91842c
ELİF BÖLÜK <elifbolukk@gmail.com>
2022-12-24 17:28:07 +0300

    Events

---
+++ Events/Product.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events // Eventler aslında bir delegate'dir. 
+{
+    public delegate void StockControl(); // Delegate tanımladık.
+    public class Product
+    {
+
+        private int _stock;
+
+        public Product(int stock) // mevcuttaki stoğu gönderiyoruz ve _stok'a atıyoruz.
+        {
+            _stock = stock;
+        }
+
+
+        public event StockControl StockControlEvent; // StockControlEvent adıyla StockControl türünde bir event tanımladık.
+        public string ProductName { get; set; } // Ürünün adını tutacak property.
+        public int Stock // Kişi stok konusunda uyarılmalıysa bu event tetiklecenektir. 
+        {
+            get { return _stock; }
+            set
+            {
+                _stock = value; // stok değeri kişinin verdiği değere eşitlendi.
+                if (_stock <= 15 && StockControlEvent != null) // stok değeri 15'e eşit veya küçük olduğunda ve StockControlEvent null değilse yani kullanıcı bu event'e abone olmuşsa event'i tetikle.
+                {
+                    StockControlEvent(); // Event tetiklendi.
+                }
+            }
+        }
+        // Stock'a değer atadığımızda StockControlEvent'i tetikliyoruz.
+
+
+        public void Sell(int amount)
+        {
+            Stock -= amount; // Stoktan satılan satış miktarını çıkarıyoruz.
+            Console.WriteLine("{1} Stock amount: {0}", Stock, ProductName); // Ekrana kalan stock adetini yazdırıyoruz. 
+        }
+    }
+}
---
+++ Events/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Product product = new Product(20); // Product nesnesi oluşturduk ve stok değerini 20 olarak atadık.
+            // product.ProductName = "Laptop"; // Ürünün adını Laptop olarak atadık.
+
+            // -------------------------------------------------- //
+            
+            // Hem harddisk hem de gsm'in satıldığı bir senaryoda;
+            
+            Product harddisk = new Product(50); // Product nesnesi "harddisk" oluşturduk ve stoktaki harddisk değerini 50 olarak atadık.
+            harddisk.ProductName = "Harddisk"; // Ürünün adını Harddisk olarak atadık.
+
+            Product gsm = new Product(50); // Product nesnesi "gsm" oluşturduk ve stoktaki gsm değerini 50 olarak atadık.
+            gsm.ProductName = "Gsm"; // Ürünün adını Gsm olarak atadık.
+            gsm.StockControlEvent += Gsm_StockControlEvent; // Gsm_StockControlEvent eventini tetiklemek için gsm.StockControlEvent'e abone olduk.
+
+            for (int i = 0; i < 10; i++)
+            {
+                harddisk.Sell(10); // harddisk satıldı.
+                gsm.Sell(10); // gsm satıldı.
+                Console.ReadLine();
+            }
+            Console.ReadLine();
+        }
+
+        private static void Gsm_StockControlEvent() // Gsm_StockControlEvent eventi tetiklendiğinde bu method çalışacak.
+        {
+            Console.WriteLine("Gsm stock is low!"); // Ekrana "Gsm stock is low!" yazdır.
+        }
+    }
+}
